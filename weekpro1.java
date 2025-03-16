bb48fcfcfed9a0ed1993ec02cd9b818750aa0e30
KARTHIKA M <km646383@gmail.com>
2025-03-16 18:49:09 +0530

    Update weekpro1.java

--- weekpro1.java
+++ weekpro1.java
@@ -5,6 +5,7 @@ class Weekpro
     public static void main(String args[])
     {
       Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the Number: ");
       String day = sc.nextLine();
       switch(day)
         {
